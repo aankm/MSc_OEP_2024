4e7026f720350fb6289442c5e023efb052d6efdd
aankm <156244807+aankm@users.noreply.github.com>
2024-01-12 23:31:30 +0100

    Aliscore

---
+++ 02-aliscore.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -S /bin/bash
+#$ -j n
+#$ -N eu_aliscore
+#$ -M a.ankermann@leibniz-lib.de
+#$ -m be
+
+
+## move to directory where results should be saved
+cd /share/pool/gbol3_eurytomidae_wgs/eury_filtering/aa_results/
+
+## path to Aliscore script
+run_aliscore="/share/pool/gbol3_eurytomidae_wgs/eury_filtering/scripts/ALISCORE_v2.0/Aliscore.02.2.pl"
+## path to input (output from 01-outlier_rem.sh)
+DIR="/share/pool/gbol3_eurytomidae_wgs/eury_filtering/aa_results"
+
+for FILE in $( ls ${DIR}/*_orm.fasta); do 
+
+	${run_aliscore} -i ${FILE}
+
+done
+
+cd /share/pool/gbol3_eurytomidae_wgs/eury_filtering
+
